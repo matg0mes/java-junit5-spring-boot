ad93eee7cdde07cbfff9c10846144781da8d3af7
matg0mes <hotmus8500@gmail.com>
2022-09-28 13:48:46 -0300

    feat(beer): create test controller

---
+++ src/test/java/guru/springframework/brewery/web/controllers/BeerControllerTest.java
@@ -0,0 +1,70 @@
+package guru.springframework.brewery.web.controllers;
+
+import guru.springframework.brewery.services.BeerService;
+import guru.springframework.brewery.web.model.BeerDto;
+import guru.springframework.brewery.web.model.BeerStyleEnum;
+import net.bytebuddy.matcher.ElementMatchers;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class BeerControllerTest {
+
+    @Mock
+    BeerService beerService;
+
+    @InjectMocks
+    BeerController beerController;
+
+    MockMvc mockMvc;
+
+    BeerDto validBeer;
+
+    @BeforeEach
+    void setUp() {
+        validBeer = BeerDto.builder()
+                .version(1)
+                .id(UUID.randomUUID())
+                .beerName("Beer1")
+                .beerStyle(BeerStyleEnum.PALE_ALE)
+                .price(new BigDecimal(12.99))
+                .quantityOnHand(4)
+                .upc(123456789012L)
+                .createdDate(OffsetDateTime.now())
+                .lastModifiedDate(OffsetDateTime.now())
+                .build();
+
+        mockMvc = MockMvcBuilders.standaloneSetup(beerController).build();
+    }
+
+    @Test
+    void testGetBeerById() throws Exception {
+        given(beerService.findBeerById(any())).willReturn(validBeer);
+
+        mockMvc.perform(get("/api/v1/beer/" + validBeer.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.id", is(validBeer.getId().toString())))
+                .andExpect(jsonPath("$.beerName", is("Beer1")));
+    }
+}
